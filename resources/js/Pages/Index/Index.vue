aa31658e2e334749e0af0b6559a34376f8f7b0c3
agavriilidis <agavriilidis@optimy.com>
2023-06-05 09:04:27 +0200

    Routes : Index and Show + Inertia responses

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import {Link} from '@inertiajs/vue3'
+</script>
+
+<template>
+    <div>Index</div>
+    <Link href="/show">Show Page</Link>
+</template>
+
+<style scoped>
+
+</style>
