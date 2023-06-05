a77b4135b9faf4b59183bf40b07ae5f44230faf1
agavriilidis <agavriilidis@optimy.com>
2023-06-05 09:20:14 +0200

    Using props for index component

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,10 +1,14 @@
 <script setup>
     import {Link} from '@inertiajs/vue3'
+    defineProps({
+        'message': String
+    });
 </script>
 
 <template>
     <div>Index</div>
     <Link href="/show">Show Page</Link>
+    <p>{{message}}</p>
 </template>
 
 <style scoped>
