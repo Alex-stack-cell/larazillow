b3dae16411f23c2224e9e510307a82e183c6d1f9
agavriilidis <agavriilidis@optimy.com>
2023-06-14 08:34:09 +0200

    Add docker volume + challenge 1 section 3

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
+        // challenge find the listing which as at least 4 beds with the cheapest price
+        dd(Listing::where('beds', '>', 4)->orderBy('beds', 'desc')->get());
         return inertia(
             'Index/Index',
             [
