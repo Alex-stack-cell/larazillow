289e2f4d803e1e0d9b1c58f86d3689bdac355adc
agavriilidis <agavriilidis@optimy.com>
2023-06-16 08:00:37 +0200

    Add fillable for Listing

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,8 +9,6 @@ class IndexController extends Controller
 {
     public function index()
     {
-        // challenge find the listing which as at least 4 beds with the cheapest price
-        dd(Listing::where('beds', '>', 4)->orderBy('beds', 'desc')->get());
         return inertia(
             'Index/Index',
             [
