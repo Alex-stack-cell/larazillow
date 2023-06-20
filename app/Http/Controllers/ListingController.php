82b94d76f55277728c0986386f214e8339bf496f
agavriilidis <agavriilidis@optimy.com>
2023-06-20 08:44:50 +0200

    add return type for controller

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
+use Inertia\ResponseFactory;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ListingController extends Controller
@@ -12,7 +14,7 @@ class ListingController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(): \Inertia\Response|\Inertia\ResponseFactory
+    public function index(): Response|ResponseFactory
     {
         return inertia(
             'Listing/Index',
@@ -25,7 +27,7 @@ class ListingController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create(): \Inertia\Response|\Inertia\ResponseFactory
+    public function create(): Response|ResponseFactory
     {
         return inertia('Listing/Create');
     }
@@ -44,7 +46,7 @@ class ListingController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Listing $listing)
+    public function show(Listing $listing): RedirectResponse
     {
         return inertia(
             'Listing/Show',
