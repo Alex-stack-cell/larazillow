8598246c7b083a00a2ef212c90f3975b92eab2d8
agavriilidis <agavriilidis@optimy.com>
2023-06-23 08:57:50 +0200

    Add conditional rendering by displaying flash message

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,8 +1,12 @@
 <script setup>
-import {Link} from '@inertiajs/vue3'
-import {ref} from 'vue'
+import {Link, usePage} from '@inertiajs/vue3'
+import {ref, computed} from 'vue'
 
 const timer = ref(0)
+const page = usePage()
+const flashSuccess = computed(() => {
+  return page.props.flash.success
+})
 setInterval(() => timer.value++, 1000)
 </script>
 
@@ -10,5 +14,15 @@ setInterval(() => timer.value++, 1000)
   <Link href="/">Main Page</Link>&nbsp;
   <Link href="/show">Show Page</Link>
   <div>The page with time {{ timer }}</div>
+  <div v-if="flashSuccess" class="success">
+    {{ flashSuccess }}
+  </div>
   <slot>Default Content if nothing provided</slot>
 </template>
+
+<style scoped>
+.success {
+    background-color: green;
+    color: white;
+}
+</style>
