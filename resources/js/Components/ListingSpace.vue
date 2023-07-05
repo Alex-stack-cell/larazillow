9619df2ba82d5e3198d05a3a146bad307d42d29f
agavriilidis <agavriilidis@optimy.com>
2023-07-05 09:04:58 +0200

    Listing details (non prop attributes)

---
+++ resources/js/Components/ListingSpace.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  listing:Object,
+})
+</script>
+
+<template>
+  <div>
+    <span class="font-bold">{{ listing.beds }} beds </span>
+    <span class="text-gray-400">|</span>&nbsp;
+    <span class="font-bold">{{ listing.baths }} baths </span>
+    <span class="text-gray-400">|</span>&nbsp;
+    <span class="font-bold">{{ listing.area }} m² </span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -2,6 +2,7 @@
 import Box from '@/Components/UI/Box.vue'
 import {Link} from '@inertiajs/vue3'
 import ListingAddress from '@/Components/ListingAddress.vue'
+import ListingSpace from '@/Components/ListingSpace.vue'
 defineProps({
   listings: Array,
 })
@@ -12,7 +13,8 @@ defineProps({
     <Box v-for="listing in listings" :key="listing.id">
       <div>
         <Link :href="route('listing.show',{listing: listing.id})">
-          <ListingAddress :listing="listing" /> ${{ listing.price }}
+          <ListingSpace :listing="listing" class="text-lg" />
+          <ListingAddress :listing="listing" class="text-gray-500" />
         </Link>
       </div>
       <div>
