5e0fbcbd8735c92c689fdeddea6b33839de40862
agavriilidis <agavriilidis@optimy.com>
2023-07-20 08:21:03 +0200

    Authentication Controller and Routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /*
+     * User creates an account **/
+    public function create()
+    {
+        //
+    }
+
+    /*
+     * User is logging in
+     * **/
+    public function store()
+    {
+        //
+    }
+
+    /*
+     * User is logging out method
+     * **/
+    public function destroy()
+    {
+
+    }
+}
